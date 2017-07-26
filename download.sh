c8cf006666c31847aaf749e9ba25ba990048f7c2
Janos Karancsi <janos.karancsi@cern.ch>
2017-07-26 12:41:26 +0200

    Add download script for automatization

--- download.sh
+++ download.sh
@@ -12,23 +12,21 @@ for arg in "$@"; do
         PRIM_DATASET=`grep "config.Data.inputDataset" $CRAB_CFG | sed "s;';;g;s;/; ;g" | awk '{ print $3 }'`
 	SITE=`grep "config.Site.storageSite" $CRAB_CFG | sed "s;';;g" | awk '{ print $3 }'`
 	# Set input/output directories
-        DL_DIR=`echo $LFN/$TASKNAME | sed "s;/store/user;/data/gridout;"`
-        TIMESTAMP=`/data/jkarancs/scripts/se_util.csh ls $SITE:$LFN/$PRIM_DATASET/$TASKNAME | tail -1`
-        SE_DIR=$LFN/$PRIM_DATASET/$TASKNAME/$TIMESTAMP
+        SE_DIR=$LFN/$PRIM_DATASET/$TASKNAME
+        DL_DIR=`echo $LFN/$PRIM_DATASET/$TASKNAME | sed "s;/store/user;/data/gridout;"`
 	# download
 	echo
 	echo "Downloading from crab config file: "$CRAB_CFG
 	echo "- Downloading from site: "$SITE
 	echo "- Destination local directory:"
 	echo "    $DL_DIR"
-	mkdir -p $DL_DIR
+	echo "- Start synchronizing:"
+	echo "    $SE_DIR"
 	echo
-        for SUBDIR in `/data/jkarancs/scripts/se_util.csh ls $SITE:$SE_DIR`; do
-	    echo "- Start downloading from:"
-	    echo "    $SE_DIR/$SUBDIR"
-	    /data/jkarancs/scripts/se_util.csh dl_mis $SITE:$SE_DIR/$SUBDIR $DL_DIR --run
-	    echo
-        done
+	for i in $( seq 1 24 ); do sleep 30m; /data/jkarancs/scripts/se_util.csh sync $SITE:$SE_DIR $DL_DIR --run; done
+	for i in $( seq 1 12 ); do sleep  1h; /data/jkarancs/scripts/se_util.csh sync $SITE:$SE_DIR $DL_DIR --run; done
+	for i in $( seq 1 12 ); do sleep  2h; /data/jkarancs/scripts/se_util.csh sync $SITE:$SE_DIR $DL_DIR --run; done
+	echo
 	echo "---- All downloads from $CRAB_CFG Done. ----"
     fi
 done
